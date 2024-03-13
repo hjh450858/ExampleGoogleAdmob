5c9d167664b18163b2bb4e459cfa87e6dc040116
hwang jae hyeon <hjh450858@gmail.com>
2024-03-13 16:07:01 +0900

    first commit

--- ExampleGoogleAdmob/Constant.swift
+++ ExampleGoogleAdmob/Constant.swift
@@ -9,8 +9,6 @@ import Foundation
 
 struct Constant {
     struct GoogleAdmobs {
-//        static let bannerKey = "ca-app-pub-4931197203551061/9964979107"
-        
         /// TEST 용 키값
         static let bannerKey = "ca-app-pub-3940256099942544/2934735716"
     }
